513aafdb426f7263ac7a97291793501980cd19b5
Liu <1366505724@qq.com>
2022-12-29 10:37:52 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { PageContainer } from "@ant-design/pro-components";
 export default function App() {
   return (
     <PageContainer>
-      {/* <Table /> */}
       <h1>控制台</h1>
       <Card />
     </PageContainer>
