41a42fe426fa54d88d2bcd655bce133a561b8178
Liu <40189175+liuda-ke@users.noreply.github.com>
2022-12-30 11:47:42 +0800

    添加mock 提交

--- src/mock.ts
+++ src/mock.ts
@@ -35,6 +35,55 @@ function getdashBoard(): dashboard[] {
     desc: "",
     title: "qBittorrent"
   });
+  dash.push({
+    avatar: "NULL",
+    url: "http://code.3han.cn",
+    content: "在线VSCode",
+    desc: "",
+    title: "Code-Server"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://remote.3han.cn",
+    content: "Jellyfin影音播放器",
+    desc: "",
+    title: "Jellyfin"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://remote.3han.cn:6063",
+    content: "Jenkins在线编译",
+    desc: "",
+    title: "Jenkins"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://remote.3han.cn:6060",
+    content: "音乐播放器",
+    desc: "",
+    title: "YesPlayMusci"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://remote.3han.cn:5083",
+    content: "rednode",
+    desc: "",
+    title: "rednode"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://docs.3han.cn",
+    content: "在线文档",
+    desc: "",
+    title: "showdoc"
+  });
+  dash.push({
+    avatar: "NULL",
+    url: "http://remote.3han.cn:5100",
+    content: "Docker在线查看",
+    desc: "",
+    title: "simpleDocker"
+  });
   return dash;
 }
 export default getdashBoard;
